46008da03c821dcaf26a3c5a0e6d96d63ff20945
Kiggz <nico.peignot@outlook.fr>
2022-10-02 18:25:50 +0200

    SearchEngine configured

--- js/SearchEngine.js
+++ js/SearchEngine.js
@@ -3,7 +3,7 @@ class SearchEngine {
     this._filterList = filterList;
     this._defaultRecipesList = recipesList;
     this._callback = callback;
-    // this._recipesList = this._defaultRecipesList;
+
   }
 
   ingredientsFilter(filters, recipes) {
